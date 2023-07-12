93c7bc88babb65482806654e5df4d38b8e4a84e1
1715leekc <1715leekc@naver.com>
2023-07-12 09:50:58 +0900

    Feat : mongoDB install

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,41 @@
 const express = require('express') //express를 설치했기 때문에 가져올 수 있다.
-const mysql = require('mysql'); //mysql을 설치했기 때문에 가져올 수 있다.
+// const mongodb = require('mongodb');
 const app = express()
+
+const port = 8080;
+// const mongoClient = mongodb.MongoClient;
+// const mongoUrl = 'mongodb://localhost:27017/test';
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-const connection = mysql.createConnection({
-  host: 'localhost:3306',     // MySQL 호스트 주소
-  user: 'root',  // MySQL 사용자 이름
-  password: '1023ldde',  // MySQL 비밀번호
-  database: 'yourdatabase'  // 사용할 데이터베이스 이름
+// app.get('/users', (req, res) => {
+//   mongoClient.connect(mongoUrl, (err, client) => {
+//     if (err) {
+//       console.error('Failed to connect to MongoDB:', err);
+//       res.status(500).send('Failed to connect to MongoDB');
+//       return;
+//     }
+
+//     const db = client.db();
+//     const collection = db.collection('users');
+
+//     collection.find().toArray((err, users) => {
+//       if (err) {
+//         console.error('Failed to retrieve users from MongoDB:', err);
+//         res.status(500).send('Failed to retrieve users from MongoDB');
+//         return;
+//       }
+
+//       res.json(users);
+//       client.close();
+//     });
+//   });
+// });
+
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
-
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL database: ' + err.stack);
-    return;
-  }
-
-  console.log('Connected to MySQL database.');
-});
-
-const data = {
-  name: 'John Doe',
-  age: 30,
-  email: 'johndoe@example.com'
-};
-
-connection.query('INSERT INTO your_table SET ?', data, (err, result) => {
-  if (err) {
-    console.error('Error executing MySQL query: ' + err.stack);
-    return;
-  }
-
-  console.log('New row inserted with ID:', result.insertId);
-});
-
-app.listen(8080)

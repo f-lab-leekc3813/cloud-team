e6f103fbc9564f4f1d2d1a445637d680549d1ce4
GYEONGBIIN <138579795+GYEONGBIIN@users.noreply.github.com>
2023-07-12 10:09:48 +0900

    Feat : Estate Start

---
+++ front/src/component/EstateBody/Content1.js
@@ -0,0 +1,32 @@
+import classes from './Content1.module.css'
+
+
+function Content1(){
+    return(
+        <section className={classes.content1_containers}>
+            <div className={classes.content1_container}>
+                <div className={classes.content1_container1}>
+                    <h1 className={classes.content1_container2}>
+                        복비없이 투명한
+                        <br/>
+                        부동산 직거래
+                    </h1>
+                    <p1 className={classes.content1_container3}>
+                        이웃이 살던 집, 당근마켓에서
+                        <br/>
+                        편하게 직거래해보세요
+                    </p1>
+                </div>
+                <div className={classes.content1_container4}>
+                    <span className={classes.content1_container5}>
+                        <img className={classes.content1_container6} src = 'images/estate/house.png' alt = '사진' />                
+                    </span>
+                </div>
+            </div>
+        </section>
+
+    
+    )
+}
+
+export default Content1;
---
+++ front/src/component/EstateBody/Content2.js
@@ -0,0 +1,22 @@
+import classes from './Content1.module.css'
+
+
+function Content2(){
+    return(
+        <section className={classes.content2_containers}>
+            <h2 className={classes.content2_container}>
+                인기 부동산 직거래 게시글
+            </h2>
+            <div className={classes.content2_container1}>
+                <div className={classes.content2_container2}>
+
+                </div>
+                
+            </div>
+        </section>
+
+    
+    )
+}
+
+export default Content2;

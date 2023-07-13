b231a29e53c0e46b64e06e1ec826cf8bcd68e7b8
1715leekc <1715leekc@naver.com>
2023-07-13 11:00:45 +0900

    Feat: 로그인 유효성 검사

--- front/src/component/SignIn.js
+++ front/src/component/SignIn.js
@@ -1,42 +1,94 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import api from "../api/api";
+
 import classes from "./SignIn.module.css";
 
 const SignIn = ({ isOpen, close }) => {
   const [email, setEmail] = useState("");
+  const [emailCheck, setEmailCheck] = useState(false);
   const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState(false);
+  const [nickname, setNickname] = useState("");
+  const [nicknameCheck, setNicknameCheck] = useState(false);
+
   const [isSignUp, setIsSignUp] = useState(false);
 
-  const loginHandler = (e) => {
-    const { name, value } = e.target;
-    if (name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
-    }
-  };
-
-  // setIsSignUp이 true면 회원가입창으로 바뀜
-  // setisSignUpdl true면 회원가입 Fost, flase면 로그인 get
-  const signUpHandler = () => {
-    setIsSignUp(!isSignUp);
+  const eamilHandler = (e) => {
+    setEmail(e.target.value);
+  }
+  const passwordHandler = (e) => {
+    setPassword(e.target.value);
+  }
+  const nicknameHandler = (e) => {
+    setNickname(e.target.value);
   }
 
-  // const loginClickHandler = () => {
-  //   fetch("", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       email,
-  //       password,
-  //     }),
-  //   })
-  //     .then((res) => res.json())
-  //     .then((res) => console.log(res));
-  // };
+  const SubmitHandler = () => {
 
+    const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
+    const passwordRegex = /^[a-zA-Z0-9]{8,}$/;
+    const nicknameRegex = /^[a-zA-Z0-9]{3,}$/;
+
+    if (emailRegex.test(email)) {
+      setEmailCheck(true);
+    }
+    if (passwordRegex.test(password)) {
+      setPasswordCheck(true);
+    }
+    if (nicknameRegex.test(nickname)) {
+      setNicknameCheck(true);
+    }
+
+
+    if (isSignUp) {
+      if (emailCheck && passwordCheck && nicknameCheck) {
+        api
+          .post("/user/signup", {
+            email: email,
+            password: password,
+            nickname: nickname,
+          })
+          .then((res) => {
+            if (res.status === 200) {
+              alert("회원가입이 완료되었습니다.");
+              close();
+            } else {
+              alert("회원가입에 실패하였습니다.");
+            }
+          })
+          .catch((err) => {
+            alert("회원가입에 실패하였습니다.");
+          });
+      } else {
+        alert("입력한 정보를 다시 확인해주세요.");
+      }
+    }else{
+      if (emailCheck && passwordCheck) {
+        api
+          .post("/user/signin", {
+            email: email,
+            password: password,
+          })
+          .then((res) => {
+            if (res.status === 200) {
+              alert("로그인이 완료되었습니다.");
+              close();
+            } else {
+              alert("로그인에 실패하였습니다.");
+            }
+          })
+          .catch((err) => {
+            alert("로그인에 실패하였습니다.");
+          });
+      } else {
+        alert("입력한 정보를 다시 확인해주세요.");
+      }
+    }
+  }
+
+  const signUpMove = () => {
+    setIsSignUp(!isSignUp);
+  }
   return (
     <>
       {isOpen ? (
@@ -47,19 +99,29 @@ const SignIn = ({ isOpen, close }) => {
             </span>
             <div className={classes.modalContents} onClick={(e) => e.stopPropagation()}>
               <img className={classes.signinIcon} src="images/logo.png" alt="애플마켓 로그인" />
+              {isSignUp ? 
+              <input
+                name="nickname"
+                className={classes.loginId}
+                type="text"
+                placeholder="닉네임을 3글자 이상 입력해주세요."
+                onChange={nicknameHandler}
+               />
+              :
+              ''}
               <input
                 name="email"
                 className={classes.loginId}
                 type="text"
-                placeholder="id"
-                onChange={loginHandler}
+                placeholder="이메일 형식으로 입력해주세요."
+                onChange={eamilHandler}
               />
               <input
                 name="password"
                 className={classes.loginPw}
                 type="password"
-                placeholder="password"
-                onChange={loginHandler}
+                placeholder="비밀번호를 8자리 이상 입력해주세요."
+                onChange={passwordHandler}
               />
               <div className={classes.loginMid}>
                 <label className={classes.autoLogin} htmlFor="hint">
@@ -67,10 +129,10 @@ const SignIn = ({ isOpen, close }) => {
                 <div className={classes.autoLogin}>아이디/비밀번호 찾기</div>
               </div>
               {isSignUp ?
-                <button onClick={signUpHandler} className={classes.loginBtn} >
+                <button onClick={SubmitHandler} className={classes.loginBtn} >
                   회원가입
                 </button> :
-                <button onClick={signUpHandler} className={classes.loginBtn} >
+                <button onClick={SubmitHandler} className={classes.loginBtn} >
                   로그인
                 </button>
                 }
@@ -85,11 +147,17 @@ const SignIn = ({ isOpen, close }) => {
                 </div>
               </div>
               <div className={classes.loginEnd}>
-                <div className={classes.loginLine}>
-                  회원이 아니신가요? 
-                </div>
-                {isSignUp ? '' :
-                <button onClick={signUpHandler}  className={classes.signUpLink}>
+                {isSignUp ? '' : 
+                  <div className={classes.loginLine}>
+                    회원이 아니신가요? 
+                  </div> 
+                  }
+                {isSignUp ? 
+                <button onClick={signUpMove}  className={classes.signUpLink}>
+                  로그인
+                </button>
+                 :
+                <button onClick={signUpMove}  className={classes.signUpLink}>
                   회원가입
                 </button>}
                 <div className={classes.noUser}>비회원 주문 조회</div>

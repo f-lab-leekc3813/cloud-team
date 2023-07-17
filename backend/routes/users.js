4344212bed8a06621d942d80bce09b7e074f17aa
ehgmlrjs <ehgmlrjs@naver.com>
2023-07-17 11:26:38 +0900

    회원가입 완료
    v1

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,68 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const connect = require('../config/connection')
-const emailCheck = require('../config/emailCheck');
-const nicknameCheck = require('../config/nicknameCheck');
+const signup = require('./signup');
+
 const { swaggerUi, specs } = require('../swagger/swagger');
 
-
-/**
- * @swagger
- * tags:
- *   name: User
- *   description: User management APIs
- */
-
-/**
- * @swagger
- * /signup:
- *   post:
- *     summary: Register a new user
- *     tags: [User]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               email:
- *                 type: string
- *               password:
- *                 type: string
- *               nickname:
- *                 type: string
- *     responses:
- *       200:
- *         description: Successful registration
- *       500:
- *         description: Internal server error
- */
 
 router.post('/signup',  function(req, res, next) {
   try {
     const { email, password, nickname } = req.body;
-    async function check(){
-      const emcheck = emailCheck(email);
-      const nicheck = nicknameCheck(nickname);
-      const em = await emcheck;
-      const ni = await nicheck;
-      if (em) {
-        return res.status(201).json({
-          message: "이미 있는 이메일"
-        })
-      } else if (ni) {
-        return res.status(202).json({
-          message: "이미 있는 닉네임"
-        })
-      } else {
-        connect(email, password, nickname)
-        return res.status(200).json({
-          message: "완료"
-        })
-      }
-    }
-    check()
+    signup(email,password,nickname,res);
   } catch (error) {
     return res.status(500).json({
       message: 'internal server error'

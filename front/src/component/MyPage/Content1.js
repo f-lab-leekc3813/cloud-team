ef3777f0df738533b6181547da29475e1942fd65
GYEONGBIIN <138579795+GYEONGBIIN@users.noreply.github.com>
2023-07-18 16:17:13 +0900

    2023-07-18

--- front/src/component/MyPage/Content1.js
+++ front/src/component/MyPage/Content1.js
@@ -1,5 +1,5 @@
 import classes from './Content1.module.css'
-
+import dummy1 from '../../data/EstateData/Content1.json'
 
 function Content1(){
     return(
@@ -10,7 +10,7 @@ function Content1(){
                     <div className={classes.content1_container9}>
                         <img className={classes.header_image1} src='/images/cloudlogo.jpg' alt='로고' />
                         <p1 className={classes.content1_container10}>
-                            구름마켓 ID
+                            구름마켓
                         </p1>
                         <img className={classes.header_image2} src='/images/cloudlogo.jpg' alt='로고' />
                     </div>
@@ -24,6 +24,12 @@ function Content1(){
                     <p>iamdoxoak@naver.com</p>
                 </div>
                 </h1>
+                <div className={classes.content1_container14}>
+                    <p1 className={classes.content1_text1}>
+                    기본정보
+                    </p1>
+
+                </div>
                 <div className={classes.content1_container12}>
                     <img className={classes.content1_image3} src='images/mypage/phone.jpg' alt='휴대전화' />
                     <span className={classes.content1_container13}>
@@ -35,7 +41,7 @@ function Content1(){
                 </div>
                 <br/>
                 <div className={classes.content1_container12}>
-                    <img className={classes.content1_image3} src='images/mypage/phone.jpg' alt='휴대전화' />
+                    <img className={classes.content1_image3} src='images/mypage/email.jpg' alt='휴대전화' />
                     <span className={classes.content1_container13}>
                         이메일 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ia******@n*******.com
                     </span>
@@ -45,7 +51,7 @@ function Content1(){
                 </div>
                 <br/>
                 <div className={classes.content1_container12}>
-                    <img className={classes.content1_image3} src='images/mypage/phone.jpg' alt='휴대전화' />
+                    <img className={classes.content1_image3} src='images/mypage/address.jpg' alt='휴대전화' />
                     <span className={classes.content1_container13}>
                         배송지 관리 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 광주광역시 북구 군왕로311
                     </span>
@@ -53,15 +59,45 @@ function Content1(){
                         수정
                     </span>
                 </div>
+                <div className={classes.content1_container15}>
+                    <p1 className={classes.content1_container16}>
+                        회원탈퇴 {">"}
+                    </p1>
+                </div>
             </div>
-            <div className={classes.content1_container4}>
-                <span className={classes.content1_container8}>
-                    <img className={classes.content1_container6} src = 'images/estate/house.png' alt = '사진' />                
-                </span>
+            <div className={classes.content1_jjim4}>
+            <h2 className={classes.content2_title}>
+                인기 부동산 직거래 게시글
+            </h2>
+            <div className={classes.content2_jjim}>
+                {dummy1.data.map((data) => {
+                    return(
+                        <div key={data.id} className={classes.content2_box}>
+                           <article className={classes.content2_box1}>
+                                <div className={classes.content2_boximage}>
+                                    <span className={classes.content2_boxspan} >
+                                        <img className={classes.content2_img1}src={data.image} alt='사진' />
+                                    </span>
+                                </div>
+                                <div className={classes.content2_boxcontent}>
+                                    <div className={classes.content2_boxname}>
+                                        {data.name}
+                                    </div>
+                                    <div className={classes.content2_boxregion}>
+                                        {data.region}
+                                    </div>
+                                    <div className={classes.content2_boxinfo1}>
+                                        {data.info1}
+                                    </div>
+                                </div>
+                           </article>
+                        </div>
+                    )
+                })}
             </div>
         </div>
+    </div>
     </section>
-
     );
 }
 

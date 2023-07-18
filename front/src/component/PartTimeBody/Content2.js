caa6492b4abf0045307d0b67cc8ba0934ed2d453
1715leekc <1715leekc@naver.com>
2023-07-18 16:49:55 +0900

    Fix : 주소
    주소를 올바르게 바꿨다

--- front/src/component/PartTimeBody/Content2.js
+++ front/src/component/PartTimeBody/Content2.js
@@ -1,15 +1,12 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
 
-import dummy1 from '../../data/partTimeData/content1.json';
-import dummy2 from '../../data/partTimeData/content2.json';
 import classes from './Content2.module.css';
-import { useEffect } from 'react';
 
 function Content2 () {
-    const [data1, setData1] = useState(null);
-    const [data2, setData2] = useState(null);
+    const [data1, setData1] = useState([]);
+    const [data2, setData2] = useState([]);
 
     useEffect(() => {
         fetchData();
@@ -17,7 +14,7 @@ function Content2 () {
 
       const fetchData = async () => {
         try {
-          const response = await axios.get('https://localhost8080/parttime');
+          const response = await axios.get('http://localhost:8080/croll/part_page');
           const responseData = response.data;
 
           const halfwayIndex = Math.ceil(responseData.length / 2);
@@ -39,9 +36,9 @@ function Content2 () {
                 인기 당근알바
             </h2>
             <div className={classes.content2_containers1}>
-                {dummy1.data.map((data) => {
+                {data1.map((data) => {
                     return(
-                        <div  key={data.id} className={classes.content2_box}>
+                        <div key={data.index} className={classes.content2_box}>
                             <Link to={`/parttime/${data.title}`}>
                                 <article className={classes.content2_box1}>
                                      <div className={classes.content2_boximage}>
@@ -81,9 +78,9 @@ function Content2 () {
                 </div>
             </div>
             <div className={classes.content2_containers2}>
-                {dummy2.data.map((data) => {
+                {data2.map((data) => {
                         return(
-                            <div key={data.id} className={classes.content2_box}>
+                            <div key={data.title} className={classes.content2_box}>
                                 <Link to={`/parttime/${data.title}`}>
                                     <article className={classes.content2_box1}>
                                             <div className={classes.content2_boximage}>

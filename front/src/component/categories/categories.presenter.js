d89b56459fa5456d29d69a7f75f3c448d1a7eb39
yerimp <theend94@naver.com>
2023-07-28 14:08:32 +0900

    Feat Banner
    움직이는 배너 1차

--- front/src/component/categories/categories.presenter.js
+++ front/src/component/categories/categories.presenter.js
@@ -1,12 +1,32 @@
 import classes from './categories.module.css';
+import { Carousel } from 'react-bootstrap';
 
 export default function CategoriesUI(props) {
     return(
     <>
-        <div className={classes.container}>
-        HTML 영역입니다(예림)
-        title,price,author,image,publisher,publishedDate,categories
-        css 는 카멜케이스로 (ex: content1Text, content1Box)
+        <div className={classes.containers}>
+            <div className={classes.container}>
+                <Carousel>
+                    <Carousel.Item>
+                        <img className={classes.slideImage} src="/images/categori/slide01.jpg" alt="wallpaper" />
+                        <Carousel.Caption>
+                        <h3>운영진이 추천하는 책</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className={classes.slideImage} src="/images/categori/slide02.jpg" alt="wallpaper" />
+                        <Carousel.Caption>
+                        <h3>가장 인기 많은 책</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className={classes.slideImage} src="/images/categori/slide03.jpg" alt="wallpaper" />
+                        <Carousel.Caption>
+                        <h3>가장 찜이 많은 책</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
         </div>
     </>
     )

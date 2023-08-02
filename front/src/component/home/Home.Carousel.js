6a0d1c9e8ff4b2327f64d221f1c3829e4995b595
GYEONGBIIN <138579795+GYEONGBIIN@users.noreply.github.com>
2023-08-02 17:29:23 +0900

    2023-08-02

--- front/src/component/home/Home.Carousel.js
+++ front/src/component/home/Home.Carousel.js
@@ -6,28 +6,7 @@ import './Content1.module.css';
 import { Carousel } from 'react-bootstrap';
 
 const MyCarousel = () => {
-  return (
-    <Carousel>
-      <Carousel.Item>
-        <img className={classes.slide_image} src='/images/main/backgroundimg1.jpg' alt='배경화면' />
-        <Carousel.Caption>
-          <h3>푸른 하늘 구름서점</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img className={classes.slide_image} src='/images/main/backgroundimg2.jpg' alt='배경화면' />
-        <Carousel.Caption>
-          <h3>금주 베스트셀러</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img className={classes.slide_image} src='/images/main/backgroundimg3.jpg' alt='배경화면' />
-        <Carousel.Caption>
-          <h3>추천 도서</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  );
+  return 
 };
 
 export default MyCarousel;

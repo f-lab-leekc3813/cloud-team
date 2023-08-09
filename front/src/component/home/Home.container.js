4d6239e50e9617cd6286ff2e91a76fb90d16bf0d
GYEONGBIIN <138579795+GYEONGBIIN@users.noreply.github.com>
2023-08-09 15:58:00 +0900

    20230809

--- front/src/component/home/Home.container.js
+++ front/src/component/home/Home.container.js
@@ -1,18 +1,23 @@
 import HomeUI from "./Home.presenter";
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { NickState } from "../../store/LoginState";
+import {useRecoilState} from "recoil";
+
 
 export default function Home() {
     const [data, setData] = useState(null);
     const [score, setScore] = useState(null);
+    const [machinelearning, setMachineLearning] = useState(null);
+    const [nick, setNick] = useRecoilState(NickState);
 
     const fetchManyBooksData = async () => {
         try {
             const response = await axios.get('http://localhost:8080/best/many_books');
             setData(response.data);
-            console.log(response.data);
+            // console.log(response.data);
         } catch (error) {
-            console.log('Failed to retrieve many_books data', error);
+            // console.log('Failed to retrieve many_books data', error);
         }
     };
 
@@ -20,21 +25,33 @@ export default function Home() {
         try {
             const response = await axios.get('http://localhost:8080/best/score_books');
             setScore(response.data);
-            console.log(response.data);
+            // console.log(response.data);
         } catch (error) {
-            console.log('Failed to retrieve score_books data', error);
+            // console.log('Failed to retrieve score_books data', error);
+        }
+    };
+
+    const fetchMachineLearningData = async () => {
+        try {
+            const response = await axios.get(`http://localhost:5000/machine/${nick}`);
+            setMachineLearning(response.data);
+            // console.log(response.data);
+        } catch (error) {
+            // console.log('Failed to retrieve score_books data', error);
         }
     };
 
     useEffect(() => {
         fetchManyBooksData();
         fetchScoreBooksData();
+        fetchMachineLearningData();
     }, []);
 
     return (
         <HomeUI
             data={data}
             score={score}
+            machinelearning={machinelearning}
         />
     );
 }

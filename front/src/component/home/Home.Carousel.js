cdcd8601c3018e0324f66569133d993de723115e
ehgmlrjs <ehgmlrjs@naver.com>
2023-08-10 17:07:06 +0900

    파일 정리
    파일 정리

--- front/src/component/home/Home.Carousel.js
+++ front/src/component/home/Home.Carousel.js
@@ -1,9 +1,5 @@
-import classes from './Home.Carousel.module.css'
-
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Content1.module.css'; 
-import { Carousel } from 'react-bootstrap';
 
 const MyCarousel = () => {
   return 

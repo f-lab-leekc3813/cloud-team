67c7876a553bcfd04a273920b477686939f6bb29
ehgmlrjs <ehgmlrjs@naver.com>
2023-08-10 09:39:04 +0900

    json data
    json data

--- backend/flask/machine_review.py
+++ backend/flask/machine_review.py
@@ -6,8 +6,13 @@ from password import password
 
 merge_df = pd.read_csv('./backend/flask/merge_df.csv')
 
-merge_df['Price'].fillna(0,inplace=True)
+merge_df['categories'] = merge_df['categories'].apply(lambda x:x.replace("'",'').replace('"',''))
 merge_df['Title'] = merge_df['Title'].apply(lambda x:x.replace('/',' '))
+merge_df['authors'].fillna("[Unknown]", inplace=True)
+merge_df['authors'] = merge_df['authors'].apply(lambda x:x.replace("'",''))
+
+merge_df['Price'].fillna(0,inplace=True)
+merge_df.fillna('',inplace=True)
 
 unique_df = merge_df.drop_duplicates('Title').sort_values('Title').reset_index(drop=True)
 
